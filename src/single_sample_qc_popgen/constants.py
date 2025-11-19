f065ed06c84656f8b9a570054023e56c82270e25
Michael Harper <michael.harper@populationgenomics.org.au>
2025-11-20 08:00:44 +1100

    Removing unused constants from constants.py

--- src/single_sample_qc_popgen/constants.py
+++ src/single_sample_qc_popgen/constants.py
@@ -1,13 +1,5 @@
 from typing import Final
 
-import cpg_utils
-from cpg_utils.config import config_retrieve, output_path
+from cpg_utils.config import config_retrieve
 
-_reads_type = config_retrieve(['workflow', 'reads_type'], default=None)
-assert _reads_type in {'fastq', 'cram'}, (
-    f'Unsupported reads type: {_reads_type}. Valid options are fastq or cram. Please set this in the configuration using [workflow][reads_type].'  # noqa: E501
-)
-READS_TYPE: Final = _reads_type.lower()
-BUCKET: Final = cpg_utils.to_path(output_path(suffix=''))
-BUCKET_NAME: Final = str(BUCKET).removeprefix('gs://').removesuffix('/')
 DRAGEN_VERSION: Final = config_retrieve(['ica', 'pipelines', 'dragen_version'])
